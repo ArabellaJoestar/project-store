aaa17b034f9da0299369a240f57fc2dc87019051
ArabellaJoestar <mateusfariassb19@gmail.com>
2025-07-05 10:07:01 -0300

    Feat: Implementação de filtro para categoria dos produtos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,9 +14,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body className="">
+      <body className="bg-gray-900">
         <header className="w-full h-30 p-3 flex items-center justify-center bg-gray-900">
-          <Link href="/" className="text-6xl duration-300" id="infLoja">Loja</Link>
+          <Link href="/" className="text-6xl duration-300 text-amber-400" id="infLoja">Loja</Link>
         </header>
         {children}
       </body>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,40 +1,79 @@
-import { Product } from "@/types"
-import Link from "next/link"
+"use client";
+
+import { useEffect, useState } from "react";
+import { Product } from "@/types";
+import Link from "next/link";
+
 async function getProducts(): Promise<Product[]> {
-  const response = await fetch('https://fakestoreapi.com/products')
-
-  const resposta = !response.ok ? (() => { throw new Error("Falha na busca de produtos") })() : await response.json()
-
-  return await resposta
+  const response = await fetch("https://fakestoreapi.com/products");
+  if (!response.ok) throw new Error("Falha na busca de produtos");
+  return await response.json();
 }
 
-export default async function Home() {
-  const products = await getProducts()
-  console.log(products)
+export default function Home() {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [selectedCategory, setSelectedCategory] = useState<string>("All");
+  const [categories, setCategories] = useState<Set<string>>(new Set());
+
+  useEffect(() => {
+    async function fetchData() {
+      const data = await getProducts();
+      setProducts(data);
+      setCategories(new Set(data.map((p) => p.category)));
+    }
+
+    fetchData();
+  }, []);
+
+  const filteredProducts =
+    selectedCategory === "All"
+      ? products
+      : products.filter((product) => product.category === selectedCategory);
 
   return (
-    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16  font-[family-name:var(--font-geist-sans)] bg-gray-900">
+    <div className="items-center justify-items-center min-h-screen p-8 pb-20 gap-16 font-[family-name:var(--font-geist-sans)] bg-gray-900">
+      <div className="flex items-center justify-around h-40 flex-col bg-blue-600 p-4 rounded-2xl">
+        <h2 className="text-4xl">Filter Category</h2>
+        <select
+          value={selectedCategory}
+          onChange={(e) => setSelectedCategory(e.target.value)}
+          className="bg-blue-950 border-none outline-none p-2 text-xl rounded-2xl text-center"
+        >
+          <option value="All">All</option>
+          {Array.from(categories).map((category, index) => (
+            <option key={index} value={category}>
+              {category}
+            </option>
+          ))}
+        </select>
+      </div>
+
       <h1 className="text-[50px]">Produtos</h1>
 
       <div className="grid grid-cols-1 grid-flow-row gap-5 md:grid-cols-3">
-        {
-          products.map((product) => (
-            <div key={product.id} className="flex items-center justify-center flex-col rounded-2xl bg-white text-gray-700 p-4 text-center hover-translateY duration-300">
-              <img src={product.image} alt={product.title} className="h-36" />
-
-              
-              <div className="mt-auto">
-                <h3 className="text-xs">{product.title}</h3>
-                <p>
-                  {product.price.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' })}
-                </p>
-              </div>
-                              <Link href={`/products/${product.id}`} className="bg-amber-400 mt-5 p-2 w-full text-xl rounded-2xl hover-spacing-shadow-btn duration-300">
-                  Ver detalhes</Link>
+        {filteredProducts.map((product) => (
+          <div
+            key={product.id}
+            className="flex items-center justify-center flex-col rounded-2xl bg-white text-gray-700 p-4 text-center hover-translateY duration-300"
+          >
+            <img src={product.image} alt={product.title} className="h-36" />
+            <div className="mt-auto">
+              <h3 className="text-xs">{product.title}</h3>
+              <p>
+                {product.price.toLocaleString("pt-BR", {
+                  style: "currency",
+                  currency: "BRL",
+                })}
+              </p>
             </div>
-          ))}
-
-
+            <Link
+              href={`/products/${product.id}`}
+              className="bg-amber-400 mt-5 p-2 w-full text-xl rounded-2xl hover-spacing-shadow-btn duration-300"
+            >
+              Ver detalhes
+            </Link>
+          </div>
+        ))}
       </div>
     </div>
   );
